d7a3d2b4898d6b5161e09e0d051f5f2da9fbc882
Steph <apoorv.gupta65@gmail.com>
2023-09-04 00:04:47 +0530

    TreeForgot

---
+++ Sep3_IsomorphicTree.java
@@ -0,0 +1,20 @@
+public class Sep3_IsomorphicTree {
+    boolean isIsomorphic(Node root1, Node root2)  { 
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        
+        if (root1.data != root2.data) {
+            return false;
+        }
+        
+        boolean check = (isIsomorphic(root1.left, root2.left) && isIsomorphic(root1.right, root2.right)) ||
+                                      (isIsomorphic(root1.left, root2.right) && isIsomorphic(root1.right, root2.left));
+        
+        return check;
+    }
+}
