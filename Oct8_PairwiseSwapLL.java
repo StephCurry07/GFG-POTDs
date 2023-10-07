ae29e83c57514ec91d095452f1d7dec07f38d048
Steph <apoorv.gupta65@gmail.com>
2023-10-08 01:53:27 +0530

    NO

---
+++ Oct8_PairwiseSwapLL.java
@@ -0,0 +1,19 @@
+public class Oct8_PairwiseSwapLL {
+    public Node pairwiseSwap(Node head){
+        Node ans = new Node(0);
+        Node temp = ans;
+        while(head != null && head.next != null){
+            Node node = head.next.next;
+            ans.next = head.next;
+            ans = ans.next;
+            ans.next = head;
+            ans = ans.next;
+            head = node;
+        }
+        if(head != null)
+            ans.next = head;
+        else
+            ans.next = null;
+        return temp.next;
+    }
+}
